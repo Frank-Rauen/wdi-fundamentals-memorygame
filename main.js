ff833aba3b7ed44e2f85634af2cfabf1c5573005
Frank-Rauen <55634877+Frank-Rauen@users.noreply.github.com>
2019-10-19 11:15:03 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+console.log("Up and running!");
+console.log("User Flipped " + cardsInPlay.push(cards[cardId]);.rank);
+const cards [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+}
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+}
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png"
+}
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png"
+	
+}
+];
+const cardsInPlay ["queen","queen","king","king"];
+
+function checkForMatch(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+};
+
+function flipCard("queen","queen","king","king")
+{console.log("User Flipped" + cards[])
+};
+
+function createBoard(for (var i = 0; i < flipCard.length; i++) {
+    // Logic here
+})
+
+var cardElement = document.createElement('img');
+cardElement.setAttribute('src', 'images/back.png');
+cardElement.setAttribute('data-id',i);
+cardElement.addEventListener('click');
+cardElement.appendChild('game-board');
+
+createBoard
+
+function flipCard.this.getAttribut('cardElement');
